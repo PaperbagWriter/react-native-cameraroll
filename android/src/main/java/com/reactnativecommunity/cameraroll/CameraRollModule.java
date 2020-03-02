fb0d997f51bebc9e707eb0cc4149a4a06d10dd65
Jeremie Bastien <jeremie.bastien@logmein.com>
2020-03-02 09:58:23 -0500

    GTCMOB-1857: Fixes lib to work on android 10

--- android/src/main/java/com/reactnativecommunity/cameraroll/CameraRollModule.java
+++ android/src/main/java/com/reactnativecommunity/cameraroll/CameraRollModule.java
@@ -76,8 +76,6 @@ public class CameraRollModule extends ReactContextBaseJavaModule {
     Images.Media.DATE_TAKEN,
     MediaStore.MediaColumns.WIDTH,
     MediaStore.MediaColumns.HEIGHT,
-    Images.Media.LONGITUDE,
-    Images.Media.LATITUDE,
     MediaStore.MediaColumns.DATA
   };
 
@@ -370,68 +368,6 @@ public class CameraRollModule extends ReactContextBaseJavaModule {
     }
   }
 
-  @ReactMethod
-  public void getAlbums(final ReadableMap params, final Promise promise) {
-    String assetType = params.hasKey("assetType") ? params.getString("assetType") : ASSET_TYPE_ALL;
-    StringBuilder selection = new StringBuilder("1");
-    List<String> selectionArgs = new ArrayList<>();
-    String bucketDisplayName = MediaStore.Images.ImageColumns.BUCKET_DISPLAY_NAME;
-    if (assetType.equals(ASSET_TYPE_PHOTOS)) {
-      selection.append(" AND " + MediaStore.Files.FileColumns.MEDIA_TYPE + " = "
-              + MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE);
-    } else if (assetType.equals(ASSET_TYPE_VIDEOS)) {
-      selection.append(" AND " + MediaStore.Files.FileColumns.MEDIA_TYPE + " = "
-              + MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO);
-      bucketDisplayName = MediaStore.Video.VideoColumns.BUCKET_DISPLAY_NAME;
-    } else if (assetType.equals(ASSET_TYPE_ALL)) {
-      selection.append(" AND " + MediaStore.Files.FileColumns.MEDIA_TYPE + " IN ("
-              + MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO + ","
-              + MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE + ")");
-    } else {
-      promise.reject(
-              ERROR_UNABLE_TO_FILTER,
-              "Invalid filter option: '" + assetType + "'. Expected one of '"
-                      + ASSET_TYPE_PHOTOS + "', '" + ASSET_TYPE_VIDEOS + "' or '" + ASSET_TYPE_ALL + "'."
-      );
-      return;
-    }
-    selection.append(") GROUP BY (").append(bucketDisplayName);
-    String[] projection = new String[]{
-            "COUNT(*) as count",
-            bucketDisplayName,
-            MediaStore.Images.ImageColumns.DATA
-    };
-    try {
-      Cursor media = getReactApplicationContext().getContentResolver().query(
-              MediaStore.Files.getContentUri("external").buildUpon().build(),
-              projection,
-              selection.toString(),
-              selectionArgs.toArray(new String[selectionArgs.size()]),
-              null);
-      if (media == null) {
-        promise.reject(ERROR_UNABLE_TO_LOAD, "Could not get media");
-      } else {
-        WritableArray response = new WritableNativeArray();
-        try {
-          if (media.moveToFirst()) {
-            do {
-              String albumName = media.getString(media.getColumnIndex(bucketDisplayName));
-              int count = media.getInt(media.getColumnIndex("count"));
-              WritableMap album = new WritableNativeMap();
-              album.putString("title", albumName);
-              album.putInt("count", count);
-              response.pushMap(album);
-            } while (media.moveToNext());
-          }
-        } finally {
-          media.close();
-          promise.resolve(response);
-        }
-      }
-    } catch (Exception e) {}
-
-  }
-
   private static void putPageInfo(Cursor media, WritableMap response, int limit, int offset) {
     WritableMap pageInfo = new WritableNativeMap();
     pageInfo.putBoolean("has_next_page", limit < media.getCount());
@@ -457,8 +393,6 @@ public class CameraRollModule extends ReactContextBaseJavaModule {
     int dateTakenIndex = media.getColumnIndex(Images.Media.DATE_TAKEN);
     int widthIndex = media.getColumnIndex(MediaStore.MediaColumns.WIDTH);
     int heightIndex = media.getColumnIndex(MediaStore.MediaColumns.HEIGHT);
-    int longitudeIndex = media.getColumnIndex(Images.Media.LONGITUDE);
-    int latitudeIndex = media.getColumnIndex(Images.Media.LATITUDE);
     int dataIndex = media.getColumnIndex(MediaStore.MediaColumns.DATA);
 
     for (int i = 0; i < limit && !media.isAfterLast(); i++) {
@@ -468,7 +402,6 @@ public class CameraRollModule extends ReactContextBaseJavaModule {
           putImageInfo(resolver, media, node, idIndex, widthIndex, heightIndex, dataIndex, mimeTypeIndex);
       if (imageInfoSuccess) {
         putBasicNodeInfo(media, node, mimeTypeIndex, groupNameIndex, dateTakenIndex);
-        putLocationInfo(media, node, longitudeIndex, latitudeIndex);
 
         edge.putMap("node", node);
         edges.pushMap(edge);
@@ -572,21 +505,6 @@ public class CameraRollModule extends ReactContextBaseJavaModule {
     node.putMap("image", image);
 
     return true;
-  }
-
-  private static void putLocationInfo(
-      Cursor media,
-      WritableMap node,
-      int longitudeIndex,
-      int latitudeIndex) {
-    double longitude = media.getDouble(longitudeIndex);
-    double latitude = media.getDouble(latitudeIndex);
-    if (longitude > 0 || latitude > 0) {
-      WritableMap location = new WritableNativeMap();
-      location.putDouble("longitude", longitude);
-      location.putDouble("latitude", latitude);
-      node.putMap("location", location);
-    }
   }
 
   /**
